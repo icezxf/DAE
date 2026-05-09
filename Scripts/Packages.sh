9fb731aa3831a185183bb6a17bb383a94fdcf6bc
icezxf <122969420+icezxf@users.noreply.github.com>
2026-05-09 09:25:25 +0000

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -62,6 +62,7 @@ UPDATE_PACKAGE() {
 UPDATE_PACKAGE "partexp" "sirpdboy/luci-app-partexp" "main"
 # UPDATE_PACKAGE "qbittorrent" "sbwml/luci-app-qbittorrent" "master" "" "qt6base qt6tools rblibtorrent"
 # UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
+PDATE_PACKAGE "luci-app-dockerman" "kenzok8/jell" "master"
 UPDATE_PACKAGE "nas" "linkease/nas-packages" "master"
 UPDATE_PACKAGE "nas_luci" "linkease/nas-packages-luci" "main"
 git clone --depth=1 https://github.com/linkease/istore-ui istore-ui
